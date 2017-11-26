6b71dce1f156870fc26df4f7a35da039918cabf9
Susana Vázquez <svazquez@netquest.com>
2017-11-26 02:19:37 +0100

    Problem 2 resolution

--- test/test_problems.py
+++ test/test_problems.py
@@ -7,10 +7,10 @@ class Problem1TestCase(unittest.TestCase):
     """Tests for `problem1.py`."""
     def setUp(self):
         self.problem1 = p1.problem1()
-        self.problem2 = p2.problem2()
+        self.problem2 = p2.problem2(4000000)
 
     def test_problem1(self):
         self.assertEqual(self.problem1, 233168), 'Error!'
 
     def test_problem2(self):
-        self.assertEqual(self.problem2, 0), 'Error!'
+        self.assertEqual(self.problem2, 4613732), 'Error!'

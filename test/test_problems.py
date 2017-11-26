206f98a3b6b2d886f96c40938e494c530ddd3ae7
Susana Vázquez <svazquez@netquest.com>
2017-11-26 01:12:14 +0100

    Added problem 2

--- test/test_problems.py
+++ test/test_problems.py
@@ -1,11 +1,16 @@
 import unittest
 from problems import p1
+from problems import p2
 
 
 class Problem1TestCase(unittest.TestCase):
     """Tests for `problem1.py`."""
     def setUp(self):
         self.problem1 = p1.problem1()
+        self.problem2 = p2.problem2()
 
     def test_problem1(self):
         self.assertEqual(self.problem1, 233168), 'Error!'
+
+    def test_problem2(self):
+        self.assertEqual(self.problem2, 0), 'Error!'
